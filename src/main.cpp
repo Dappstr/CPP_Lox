992e1e46f083a807ba6aa36c98506b9c37882e00
Dappster <87837549+Dappstr@users.noreply.github.com>
2024-08-12 16:03:20 -0700

    Initial commit. Reading in from file and reading from REPLIT

---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+static bool had_error = false;
+
+void report(const size_t line, const std::string& where, const std::string& msg) {
+    std::cout << "[line: " << line << "] Error " << where << ": " << msg << '\n';
+    had_error = true;
+}
+
+void error(const size_t line, const std::string& message) {
+    report(line, "", message);
+}
+
+void run(const std::string& src) {
+/*    Scanner scanner = Scanner(src);
+    std::vector<Token> tokens = scanner.scan_tokens();
+
+    for(const Token& t : tokens) {
+        std::cout << t << '\n';
+    }
+    */
+}
+
+void run_file(const char* file_loc) {
+    std::fstream file {file_loc, std::ios::in};
+    if(!file.is_open()) {
+        std::cerr << "Error opening file: " << file_loc;
+        had_error = true;
+        return;
+    }
+    file.seekg(0, std::ios::end);
+    const size_t file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::string buffer(file_size, '\0');
+
+    file.read(&buffer[0], file_size);
+    file.close();
+
+    // std::cout << "File contents: " << buffer;
+}
+
+void run_prompt() {
+    while(true) {
+        std::cout << "> ";
+        std::string line {};
+        std::getline(std::cin, line);
+
+        if(line == "quit()") {
+            break;
+        }
+        else {
+            run(line);
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        run_prompt();
+    }
+    else if(argc == 2) {
+        run_file(argv[1]);
+    }
+    else {
+        std::cerr << "Usage: " << argv[0] << " [script]";
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
